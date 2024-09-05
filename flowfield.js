f43d6c481f974b8c6c9c7accf92c1a18d0331279
olgaprylutska <147548203+olgaprylutska@users.noreply.github.com>
2024-09-05 21:53:18 +0200

    moving particles

--- flowfield.js
+++ flowfield.js
@@ -1,24 +1,18 @@
-let scale = 20;
-let cols, rows;
-let flowField = [];
 let particles = [];
 
 function setup() {
   createCanvas(800, 800);
-  cols = floor(width / scale);
-  rows = floor(height / scale);
-  flowField = new Array(cols * rows);
+  background(0);
 
   for (let i = 0; i < 300; i++) {
     particles[i] = new Particle();
   }
-
-  background(0);
 }
 
 function draw() {
+  background(0, 25);
+
   for (let particle of particles) {
-    particle.follow(flowField);
     particle.update();
     particle.show();
   }
@@ -28,27 +22,10 @@ class Particle {
   constructor() {
     this.pos = createVector(random(width), random(height));
     this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.maxSpeed = 2;
-  }
-
-  follow(vectors) {
-    let x = floor(this.pos.x / scale);
-    let y = floor(this.pos.y / scale);
-    let index = x + y * cols;
-    let force = vectors[index];
-    this.applyForce(force);
-  }
-
-  applyForce(force) {
-    this.acc.add(force);
   }
 
   update() {
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxSpeed);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
+    this.pos.add(p5.Vector.random2D());
   }
 
   show() {
