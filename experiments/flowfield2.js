07231555ce738f0546ba3aaabda467d3913e8cee
olgaprylutska <147548203+olgaprylutska@users.noreply.github.com>
2024-09-08 18:18:50 +0200

    .

--- experiments/flowfield2.js
+++ experiments/flowfield2.js
@@ -1,42 +1,44 @@
-let particles = [];let inc = 0.1;
-let scale = 20;
-let cols;
-let rows;
+let particles = [];
 let flowField;
+let cols, rows;
+let scale = 20;
+let inc = 0.1;
+let noiseStrength = 0.05;
 
 function setup() {
   createCanvas(800, 800);
+  background(255, 255, 255);
   cols = floor(width / scale);
   rows = floor(height / scale);
   flowField = new Array(cols * rows);
 
-
   for (let i = 0; i < 300; i++) {
     particles[i] = new Particle();
   }
-  background(0);
 }
 
 function draw() {
   let yOffset = 0;
   for (let y = 0; y < rows; y++) {
-    let xOffset = 0;
+    let xOffset = 90;
     for (let x = 0; x < cols; x++) {
       let index = x + y * cols;
-      let angle = noise(xOffset, yOffset) * TWO_PI * 4; 
+      let angle = noise(xOffset, yOffset) * TWO_PI * 4;
       let vector = p5.Vector.fromAngle(angle);
-      flowField[index] = vector; 
+      vector.setMag(1);
+      flowField[index] = vector;
       xOffset += inc;
     }
     yOffset += inc;
   }
 
- 
   for (let particle of particles) {
     particle.follow(flowField);
     particle.update();
+    particle.applyRandomForce();
     particle.edges();
     particle.show();
+    particle.checkReset();
   }
 }
 
@@ -61,19 +63,22 @@ class Particle {
     this.acc.add(force);
   }
 
+  applyRandomForce() {
+    let randomForce = createVector(random(-noiseStrength, noiseStrength), random(-noiseStrength, noiseStrength));
+    this.applyForce(randomForce);
+  }
+
   update() {
     this.vel.add(this.acc);
     this.vel.limit(this.maxSpeed);
     this.pos.add(this.vel);
-    this.acc.mult(0); 
+    this.acc.mult(0);
   }
 
-  edges() {
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
-
+  show() {
+    stroke(147, 151, 153, 50);
+    strokeWeight(2);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
     this.updatePrev();
   }
 
@@ -82,11 +87,29 @@ class Particle {
     this.prevPos.y = this.pos.y;
   }
 
-  show() {
-    stroke(255, 255, 255); 
-    strokeWeight(1); 
-    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y); 
-    this.updatePrev();
+  edges() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  }
+
+  checkReset() {
+    if (random(1) < 0.001) {
+      this.pos = createVector(random(width), random(height));
+      this.updatePrev();
+    }
   }
 }
-
