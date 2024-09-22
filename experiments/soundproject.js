ceab655ecb29181fafa7b734e956b1bea04ebea3
olgaprylutska <147548203+olgaprylutska@users.noreply.github.com>
2024-09-22 17:45:47 +0200

    added sound project

--- experiments/soundproject.js
+++ experiments/soundproject.js
@@ -1,40 +1,89 @@
+let particles = [];
 let synth;
-let volume;
-let filter;
-let reverb;
-let oscillator;
+let lastNoteTime = 0; 
+const noteInterval = 200; 
+let started = false; 
 
+function setup() {
+  createCanvas(800, 800);
+  background(208, 138, 192);
 
-function setup (){
-    createCanvas (800,800);
-    background (210,105,30);
-    textAlign(CENTER,CENTER);
-    textSize(24);
-    text('CLICK TO PLAY SOUND', width / 2, height / 2);
+  
+  synth = new Tone.Synth({
+    oscillator: { type: "triangle" }, 
+    envelope: {
+      attack: 0.2,  
+      decay: 0.3,   
+      sustain: 0.4, 
+      release: 0.6, 
+    },
+  });
 
-    synth = new Tone.Synth({
-        oscillator: {
-          type: "sine",
-        },
-      });
+  
+  volume = new Tone.Volume(0); 
+  filter = new Tone.Filter(500, "lowpass"); 
+  reverb = new Tone.Reverb({ decay: 1.5, wet: 0.4 }); 
+  
+  synth.chain(volume, filter, reverb, Tone.Destination);
 
-      volume = new Tone.Volume(-12);
-      
-      filter = new Tone.Filter(800, "bandpass");
-    
-      reverb = new Tone.Reverb({
-        decay: 3, 
-        wet: 0.5, 
-      });
+  
+  mousePressed = () => {
+    Tone.start();
+    started = true; 
+  };
 
-      synth.chain(volume, filter, reverb, Tone.Destination);
+  for (let i = 0; i < 300; i++) {
+    particles[i] = new Particle();
+  }
 }
 
-function mousePressed() {
-     Tone.start();
-     synth.triggerAttackRelease("C5", "8n");
+function draw() {
+  background(192, 107, 172);
 
-     setTimeout(() => synth.triggerAttackRelease("B5", "8n"), 200); 
-     setTimeout(() => synth.triggerAttackRelease("C5", "8n"), 400); 
-     setTimeout(() => synth.triggerAttackRelease("A5", "8n"), 200); 
-     }
+  for (let particle of particles) {
+    particle.update();
+    particle.show();
+  }
+}
+
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+  }
+
+  update() {
+    this.pos.add(p5.Vector.random2D());
+
+    
+    if (started) { 
+      let currentTime = millis();
+      if (currentTime - lastNoteTime > noteInterval) {
+        if (this.pos.y < height / 2) {
+          this.playSound("C4"); 
+        } else {
+          this.playSound("E4"); 
+        }
+        lastNoteTime = currentTime; 
+      }
+    }
+
+    
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.y > height) this.pos.y = 0;
+  }
+
+  playSound(note) {
+    
+    synth.triggerAttackRelease(note, "8n");
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    point(this.pos.x, this.pos.y);
+  }
+}
+
